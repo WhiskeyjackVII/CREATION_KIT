8721b884f26349c53a5fadc195425fed2512dc8d
WhiskeyjackVII <euanbetts@live.ie>
2015-04-08 15:31:03 +0100

    end game menus and actions

--- public_html/js/Framework/Button.js
+++ public_html/js/Framework/Button.js
@@ -29,7 +29,11 @@ function Button(image){
     this.image.onload = function(){
         setWidth(this.width);
         setHeight(this.height);
-        setX((CANVAS_WIDTH/2) - (width/2));
+        if(getX() > 0){
+            
+        }else{
+            setX((CANVAS_WIDTH/2) - (width/2));
+        }
     };
     
     this.setY = function(newY){
@@ -38,8 +42,14 @@ function Button(image){
     var setX = function(newX){
         x = newX;
     };
+    this.setX = function(newX){
+        x = newX;
+    };
     
     this.getX = function(){
+        return x;
+    };
+    var getX = function(){
         return x;
     };
     this.getY = function(){
